4a15f82591606865d0154b30f1eb34ddee651ce1
ajhool <ajh022@bucknell.edu>
2017-04-03 01:49:28 -0400

    First cut at microservice authentication middleware

--- app/middleware/authenticate.js
+++ app/middleware/authenticate.js
@@ -1,31 +1,36 @@
 /* @flow */
 
-import jwt from 'jsonwebtoken'
 import Constants from '../config/constants'
+import requestPromise from 'request-promise'
 
-const { sessionSecret } = Constants.security
-
-//TODO: Authenticate access to all posts with this user's ID, but we don't
-//      Need the actual user to search for it here.
+/*
+ * Performs a get request to the user store to check the Authorization header and retrieve the userId.
+ */
 
 export default function authenticate(req, res, next) {
   const { authorization } = req.headers
-  jwt.verify(authorization, sessionSecret, async (err, decoded) => {
-    if (err) {
-      return res.sendStatus(401)
-    }
 
-    // If token is decoded successfully, find user and attach to our request
-    // for use in our route or other middleware
-    try {
-      const user = await User.findById(decoded._id)
-      if (!user) {
-        return res.sendStatus(401)
-      }
-      req.currentUser = user
-      next()
-    } catch(err) {
-      next(err)
-    }
+  const options = {
+    uri: Constants.userStore.url,
+    headers: {
+        'User-Agent': 'Request-Promise',
+        'Authorization': authorization
+    },
+    json: true // Automatically parses the JSON string in the response
+  }
+
+  requestPromise(options)
+    .then(function (user) {
+        if(user){
+         req.currentUser = user
+         next()
+       } else {
+         res.status(401).send()
+       }
+    })
+    .catch(function (err) {
+        // API call failed...
+      res.status(500).send({message: unable to process request})
+    })
   })
 }
